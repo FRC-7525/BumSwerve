46178a2f360e1cb70d530db7a9d118a83a1e573f
Otto <grentomato@gmail.com>
2024-10-08 16:06:45 -0400

    Kraken sim layer

---
+++ src/main/java/frc/robot/pioneersLib/bumSwerve/SwerveMotorIOs/SwerveMotorIOKrakenSim.java
@@ -0,0 +1,101 @@
+package frc.robot.pioneersLib.bumSwerve.SwerveMotorIOs;
+
+import com.ctre.phoenix6.configs.FeedbackConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.configs.TalonFXConfigurator;
+import com.ctre.phoenix6.controls.PositionVoltage;
+import com.ctre.phoenix6.controls.VelocityVoltage;
+import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.sim.TalonFXSimState;
+
+import edu.wpi.first.math.system.plant.DCMotor;
+import edu.wpi.first.wpilibj.simulation.DCMotorSim;
+
+public class SwerveMotorIOKrakenSim implements SwerveMotorIO {
+
+    private TalonFX dummyTalon;
+    private TalonFXSimState talonController;
+    private TalonFXConfigurator configurator;
+
+    private DCMotorSim motorSim;
+    private Slot0Configs configs;
+
+    private double gearing;
+    private boolean isDrive;
+    
+    public SwerveMotorIOKrakenSim(int placeholderCanID, boolean isDrive, double gearRatio) {
+        dummyTalon = new TalonFX(placeholderCanID);
+        talonController = dummyTalon.getSimState();    
+        configurator = dummyTalon.getConfigurator();
+
+        // Note: These MOI values are for L3+ SDS Swerve Modules w kraken drive & neo turn
+        motorSim = new DCMotorSim(DCMotor.getKrakenX60(1), gearing, isDrive ? 0.000520786 : 0.00062093);
+        motorSim.update(0.02);
+
+        configs = new Slot0Configs();
+
+        // TODO: See if dummy PID is uneeded
+        configs.kP = 0.0;
+        configs.kI = 0.0;
+        configs.kD = 0.0;
+        configurator.apply(configs);
+
+        // Take into account gear ratio
+        FeedbackConfigs feedback = new FeedbackConfigs();
+        feedback.SensorToMechanismRatio = gearRatio;
+        configurator.apply(feedback);
+
+        this.gearing = gearRatio;
+        this.isDrive = isDrive;
+    }  
+
+    @Override
+    public void setVoltage(double volts) {
+        dummyTalon.setVoltage(volts);
+    }
+    
+    @Override
+    public void configurePID(double kP, double kI, double kD) {
+        configs.kP = kP;
+        configs.kI = kI;
+        configs.kD = kD;
+
+        configurator.apply(configs);
+    }
+
+    public void configureFF(double kV, double kS) {
+        configs.kV = kV;
+        configs.kS = kS;
+
+        configurator.apply(configs);
+    }
+
+    @Override
+    public void setVelocity(double velocityRPS) {
+        if (!isDrive) throw new Error("Cannot use setVelocity with a turn motor");
+
+        VelocityVoltage command = new VelocityVoltage(velocityRPS).withSlot(0);
+        dummyTalon.setControl(command);
+        motorSim.setInputVoltage(talonController.getMotorVoltage());
+        talonController.setRawRotorPosition(motorSim.getAngularPositionRotations());
+        talonController.setRotorVelocity(motorSim.getAngularVelocityRPM()/60);
+    }
+
+    @Override
+    public void setPosition(double positionDeg) {
+        if (isDrive) throw new Error("Cannot use setPosition with a drive motor");
+
+        PositionVoltage command = new PositionVoltage(positionDeg/360).withSlot(0);
+        dummyTalon.setControl(command);
+        motorSim.setInputVoltage(talonController.getMotorVoltage());
+        talonController.setRawRotorPosition(motorSim.getAngularPositionRotations());
+        talonController.setRotorVelocity(motorSim.getAngularVelocityRPM()/60);
+    }
+
+    @Override
+    public void setBrakeMode(boolean enable) {
+        dummyTalon.setNeutralMode(enable ? NeutralModeValue.Brake : NeutralModeValue.Coast);
+    }
+
+}
