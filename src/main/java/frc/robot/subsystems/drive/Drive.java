a25513e1b792f256197597028bdee2945dc098d6
Otto <grentomato@gmail.com>
2024-10-09 12:56:01 -0400

    aaa

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -46,7 +46,7 @@ public class Drive extends Subsystem<DriveStates> {
         // TODO: Tune
         drive.configureAnglePID(1, 0, 0);
         drive.configureDrivePID(1, 0, 0);
-        drive.configureDriveFF(0, 1);
+        // drive.configureDriveFF(0, 1);
     }
 
     public void runState() {
