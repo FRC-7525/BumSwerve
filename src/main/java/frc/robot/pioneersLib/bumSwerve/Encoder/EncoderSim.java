7c97a0eddc68549be5e3851398c502c36bb8af66
PotmanNob <Potmannob@outlook.com>
2024-10-09 20:19:30 -0500

    i have no clue on what im doing for sim

---
+++ src/main/java/frc/robot/pioneersLib/bumSwerve/Encoder/EncoderSim.java
@@ -0,0 +1,33 @@
+package frc.robot.pioneersLib.bumSwerve.Encoder;
+
+
+public class EncoderSim {
+    int ID;
+    double encoderRotationDeg;
+    double encoderOffset;
+    boolean inverted;
+
+    EncoderSim(int ID) {
+        this.ID = ID;
+        encoderRotationDeg = 0;
+        encoderOffset = 0;
+        inverted = false;
+    }
+
+    public void setRotationDeg(double deg) {
+        this.encoderRotationDeg = deg;
+    }
+
+    public void setEncoderOffset (double deg) {
+        this.encoderOffset = deg;
+    }
+
+    public double getRotationDeg() {
+        return this.encoderRotationDeg;
+    }
+
+    public void setInverted(boolean inverted) {
+        this.inverted = inverted;
+    }
+
+}
