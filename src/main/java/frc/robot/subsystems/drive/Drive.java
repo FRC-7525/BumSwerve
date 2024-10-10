0273f0e3add2cc7b5dc56bbd532446dfa2368a25
Otto <grentomato@gmail.com>
2024-10-10 18:20:50 -0400

    P not 0

--- src/main/java/frc/robot/subsystems/drive/Drive.java
+++ src/main/java/frc/robot/subsystems/drive/Drive.java
@@ -79,7 +79,7 @@ public class Drive extends Subsystem<DriveStates> {
 
         drive = new SwerveDrive(TRACK_WIDTH_X, TRACK_WIDTH_Y, modules, gyroIO, MAX_SPEED, WHEEL_RADIUS, sim);
         // TODO: Tune
-        drive.configureAnglePID(0.0, 0, 0.0);
+        drive.configureAnglePID(0.1, 0, 0.0);
         drive.configureDrivePID(0.01, 0, 0);
     }
 
