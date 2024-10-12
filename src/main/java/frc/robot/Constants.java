5896db10213309c2164808cc25092b5b4b659524
Otto <grentomato@gmail.com>
2024-10-11 23:18:59 -0400

    Remove unuse import

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -10,7 +10,6 @@ import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.numbers.N1;
 import edu.wpi.first.math.numbers.N3;
 import edu.wpi.first.math.util.Units;
-import edu.wpi.first.units.Unit;
 
 public class Constants {
     enum RobotState {
