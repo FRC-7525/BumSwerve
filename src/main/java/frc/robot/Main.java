ce33ef90b055d27ff95e603d689d46de1702dc56
Otto <grentomato@gmail.com>
2024-10-31 00:06:52 -0400

    hhehehe modify main.java

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
+import frc.robot.pioneersLib.CI.Crash;
 
 /**
  * Do NOT add any static variables to this class, or any initialization at all. Unless you know what
@@ -12,14 +13,30 @@ import edu.wpi.first.wpilibj.RobotBase;
  * call.
  */
 public final class Main {
-  private Main() {}
+    private Main() {}
 
-  /**
-   * Main initialization function. Do not perform any initialization here.
-   *
-   * <p>If you change your main robot class, change the parameter type.
-   */
-  public static void main(String... args) {
-    RobotBase.startRobot(Robot::new);
-  }
+    /**
+     * Main initialization function. Do not perform any initialization here.
+     *
+     * <p>If you change your main robot class, change the parameter type.
+     */
+    public static void main(String... args) {
+        if (isRunningInCI()) {
+            // If running in CI, run the Crash class
+            Crash.main(args);
+        } else {
+            // Otherwise, start the normal robot
+            RobotBase.startRobot(Robot::new);
+        }
+    }
+
+    /**
+     * Checks if the code is running in a CI environment. (uh or like its just for the crash check but wtv)
+     *
+     * @return true if running in a CI environment; false otherwise.
+     */
+    private static boolean isRunningInCI() {
+        // Check if CI_NAME environment variable is set to "Crash"
+        return "Crash".equals(System.getenv("CI_NAME"));
+    }
 }
