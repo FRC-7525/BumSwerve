bbf0c1d2ff17bbbab2de4d843ded0f01aa2ecddf
Otto <grentomato@gmail.com>
2024-11-05 15:10:15 -0600

    We can print things again

--- src/main/java/frc/robot/pioneersLib/bumSwerve/SwerveModule.java
+++ src/main/java/frc/robot/pioneersLib/bumSwerve/SwerveModule.java
@@ -7,7 +7,6 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
-import edu.wpi.first.math.proto.System;
 import edu.wpi.first.math.util.Units;
 import frc.robot.pioneersLib.bumSwerve.SwerveAbsoluteEncoder.SwerveAbsoluteEncoderIO;
 import frc.robot.pioneersLib.bumSwerve.SwerveAbsoluteEncoder.SwerveAbsoluteEncoderIOInputsAutoLogged;
